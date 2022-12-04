d2094cb5b5ada58d917d47c65099106d160c8b51
Wiktor <Wiktor@DESKTOP-8DRIGKV>
2022-12-04 23:10:59 +0100

    unsupported platforms removed

--- Offline9GagDownloader/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Offline9GagDownloader;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
